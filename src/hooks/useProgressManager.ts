42cd712027b8498125398f2a5750bd4dd4f6a8ff
1718wani <ikuya1293@gmail.com>
2023-10-29 22:20:20 +0900

    Timerの値をUseManagerを使って変えられるようにしました。

--- src/hooks/useProgressManager.ts
+++ src/hooks/useProgressManager.ts
@@ -5,14 +5,17 @@ import type { ProgressStatus } from "~/types/AllTypes";
 type UseProgressOptions = {
   initialProgressValue: number;
   targetProgressValue: number;
-  onReachZero: () => void;
-  
+  duration:number;
+  onReachZero?: () => void;
+  onReachCountingUpTarget?: () => void;
 };
 
 export const UseProgressManager = ({
   initialProgressValue, // 最初のProgressValue
   targetProgressValue, // 目指すprogressValue
+  duration,
   onReachZero, // ０になったときに呼び出すメソッド
+  onReachCountingUpTarget, // カウントアップ時にprogressValue>=targetProgressValueになった際呼び出し
 }: UseProgressOptions) => {
   const [progressValue, setProgressValue] = useState(initialProgressValue);
   const [progressStatus, setProgressStatus] =
@@ -22,7 +25,9 @@ export const UseProgressManager = ({
     () => {
       if (progressStatus === "isCountingDown") {
         if (progressValue === 0) {
-          onReachZero();
+          if (onReachZero) {
+            onReachZero();
+          }
         }
         if (progressValue > targetProgressValue) {
           setProgressValue((prev) => prev - 1);
@@ -33,11 +38,15 @@ export const UseProgressManager = ({
         if (progressValue < targetProgressValue) {
           setProgressValue((prev) => prev + 1);
         } else {
+          if (onReachCountingUpTarget) {
+            onReachCountingUpTarget();
+            
+          }
           setProgressStatus("neutral");
         }
       }
     },
-    progressStatus !== "neutral" ? 30 : null 
+    progressStatus !== "neutral" ? duration : null
   );
 
   return {
@@ -46,5 +55,6 @@ export const UseProgressManager = ({
     progressStatus,
     setProgressStatus,
     onReachZero,
+    onReachCountingUpTarget,
   };
 };
